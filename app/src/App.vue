a6b14e584c0be48f01d80befb3d66ca9d7baa3b7
Kimberley09 <kimberleyc8@nycstudents.net>
2025-02-10 13:45:00 -0500

    zsdf

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,8 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
@@ -83,3 +82,4 @@ nav a:first-of-type {
   }
 }
 </style>
+
